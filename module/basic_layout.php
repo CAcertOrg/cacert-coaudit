71c1ba5d5f621efdc03658067bea7729d0101eca
Martin Gummi <martin.gummi@cacert.org>
2014-06-04 08:26:00 +0200

    master: add styling

--- module/basic_layout.php
+++ module/basic_layout.php
@@ -1,28 +1,79 @@
 <?php
 
 function titlebar(){
-    $tabstrings = '<div class="headerbar">';
-    $tabstrings .= '<h1>' . _('Co-Audit system') . '</h1>';
-    $tabstrings .= '</div >';
+    $coauditsystem = _('Co-Audit system');
+    $tabstrings = <<<foohtmlheader
+    <header class="mainHeader">
+    <div class="headerbar">
+    <img class="logo" src="res/img/CAcert-logo-colour-210.png"/>
+    <h1>$coauditsystem</h1>
+    </div>
+foohtmlheader;
 
     return $tabstrings;
 }
 
 function footer(){
+    $tabstrings = '';
+    /*
     $tabstrings = '<div class="footerbar">';
     $tabstrings .= '<h3>Links to mmmm</h3>';
     $tabstrings .= '</div >';
-
+    */
     return $tabstrings;
 }
 
 function menu(){
-    $tabstrings = '<div class="menubar">';
+    $statics = _('Statics');
+    $overview = _('Overview');
+    $coauditors = _('Co-Auditors');
+    $newEntry = _('Enter new entry');
+    $ownEntry = _('List own entries');
+    //$Admin
+
+    $tabstrings = <<<foohtmlnav
+		<nav>
+		<!--div class="menubar"-->
+		    <ul>
+			<li class="cat1">
+				<a href="#">$statics</a>
+			    <ul>
+				<li><a href="index.php?type=statistic">$overview</a></li>
+			    </ul>
+			</li>
+
+			<li class="cat2"><a href="#">$coauditors</a>
+			    <ul>
+				<li><a href="index.php?type=result">$newEntry</a></li>
+				<li><a href="#">$ownEntry</a></li>
+			    </ul>
+			</li>
+
+			<li class="cat3"><a href="#"><span>Admin</span></a>
+			    <ul class="sub-menu">
+				<li><a href="index.php?type=userlist">List user</a></li>
+				<li><a href="index.php?type=sessionlist">List sessions</a></li>
+				<li><a href="index.php?type=topiclist">List topics</a></li>
+			    </ul>
+			</li>
+
+			<li class="cat4"><a href="#">Login</a></li>
+
+			<li class="cat5"><a href="#">Logout</a></li>
+		    </ul>
+			<!--/div-->
+		</nav>
+foohtmlnav;
+
+
+/*
+    $tabstrings = '<nav>';
+    //$tabstrings = '<div class="menubar">';
     $tabstrings .= '<ul>';
-    $tabstrings .= '<li>' . _('Statics') . '</li>';
+    $tabstrings .= '<li class="cat1">' . _('Statics');
     $tabstrings .=      '<ul>';
-    $tabstrings .=      '<li><a href="index.php?type=statistic">' . _('Overview') . '</a></li>';
-    $tabstrings .=      '</ul>';
+    $tabstrings .=      '<li><a href=" ">' . _('Overview') . '</a></li>';
+    $tabstrings .=      '</ul></li>';
 
     $tabstrings .= '<li>' . _('Coauditors') . '</li>';
     $tabstrings .=      '<ul>';
@@ -41,23 +92,26 @@ function menu(){
     $tabstrings .= '<li><a href="#">' . _('Login') . '</a></li>';
     $tabstrings .= '<li><a href="#">' . _('Logout') . '</a></li>';
     $tabstrings .= '</ul>';
-    $tabstrings .= '</div >';
-
+    $tabstrings .= '</nav>';
+    //$tabstrings .= '</div >';
+*/
     return $tabstrings;
 }
 
 function headerstart($title){
-    $tabstrings = '<html >';
-    $tabstrings .= '<head >';
+    $tabstrings = '<!DOCTYPE html>';
+    $tabstrings .= '<html lang="en">';
+    $tabstrings .= '<head>';
     $tabstrings .= '<title>' . _('CAcert Coaudit'). $title . '</title>';
-    $tabstrings .= '<link rel="stylesheet" href="coaudit.css" type="text/css" />';
-    $tabstrings .= '</head >';
-    $tabstrings .= '<body>';
+    $tabstrings .= '<link rel="stylesheet" href="res/css/template.css" type="text/css" />';
+    $tabstrings .= '<meta name=content="width=device=width, initial-scale=1.0"/>';
+    $tabstrings .= '</head>';
+    $tabstrings .= '<body class="body">';
     return $tabstrings;
 }
 
 function footerend(){
-    $tabstrings = '</body >';
+    $tabstrings = '</body>';
     $tabstrings .= '</html>';
     return $tabstrings;
 
