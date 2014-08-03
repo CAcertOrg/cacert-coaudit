9dbebb83e3c842b76fe2afdd87b3b636f6e0f4d2
inopiae <inopiae@cacert.org>
2014-08-03 08:52:05 +0200

    master: worked on log output

--- module/basic_functions.php
+++ module/basic_functions.php
@@ -117,7 +117,7 @@ function test_data(){
 function write_log($file, $id, $content){
     $filename ="../log/" . $file . "_" .  date('Y_m') . ".log";
     $f = fopen($filename, 'a');
-    $output = date('Y_m_d_H_i_s') . ', ' . $_SESSION['user']['id'] . ', $id, "' . $content;
+    $output = date('Y_m_d_H_i_s') . ', ' . $_SESSION['user']['id'] . ', $id, ' . $content . '\r\n';
     fwrite($f, $output);
     fclose($f);
 }
