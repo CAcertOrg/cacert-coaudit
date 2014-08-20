db669fa29352217a275973247af74ed70c63765a
inopiae <inopiae@cacert.org>
2014-08-20 19:27:42 +0200

    master: added kpi handling part2

---
+++ forms/kpi.php
@@ -0,0 +1,58 @@
+<?php
+
+include_once('../module/output_functions.php');
+include_once('../module/class.db_functions.php');
+
+$db = new db_function();
+//Check access to page
+$readperm = get_read_permission('topic');
+$writeperm = get_write_permission('topic');
+
+if (isset($_REQUEST['kid'])) {
+    $kid = intval($_REQUEST['kid']);
+} else {
+    $kid = 0;
+}
+
+if ($kid == 0) {
+    //new kpi
+    $session_id = '';
+    $session_year = '';
+    $assurances = '';
+    $target = '';
+} else {
+    //edit kpi
+    $kpi = $db -> get_all_kpi(" coaudit_refdata_id = $kid");
+echo var_dump($kpi) . '<br/>';
+    $session_id = $kpi['session_id'];
+    $session_year = $kpi['session_year'];
+    $assurances = $kpi['assurances'];
+    $target = $kpi['target'];
+}
+
+//refresh refresh
+
+$sessionres = $db -> get_all_session();
+
+$hidden[]=array('kid',$kid);
+
+//buildform
+echo start_div('content');
+
+if ($readperm == 0) {
+    echo error(_('You do not have the right to read this page.'));
+    exit;
+}
+
+echo built_form_header('../www/index.php?type=kpi');
+echo tableheader(_('KPI'), 2);
+echo tablerow_2col_dropbox(_('Coaudit session'), $sessionres, $session_id, 'session_id', 'session_name', 0);
+echo tablerow_2col_textbox(_('Year'), 'session_year', $session_year);
+echo tablerow_2col_textbox(_('Assurance'), 'assurances', $assurances);
+echo tablerow_2col_textbox(_('Target [%]'), 'target', $target);
+echo tablefooter_user(2, $kid, $writeperm);
+echo built_form_footer($hidden);
+echo end_div();
+
+
+?>
---
+++ forms/kpilist.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once('../module/output_functions.php');
+include_once('../module/class.db_functions.php');
+
+$db = new db_function();
+//Check access to page
+$readperm = get_read_permission('kpilist');
+$writeperm = get_write_permission('kpilist');
+
+
+//get data
+$kpis = $db -> get_all_kpi();
+
+
+echo start_div('content');
+
+if ($readperm == 0) {
+    echo error(_('You do not have the right to read this page.'));
+    exit;
+}
+
+echo tableheader(_('KPI list'), 4);
+echo tablerow_kpilist_header();
+
+if (count($kpis) <= 0 ) {
+    echo tablerow_no_entry(2);
+} else {
+    foreach ($kpis as $kpi) {
+        echo tablerow_kpilist($kpi);
+    }
+}
+
+if ($writeperm > 0) {
+    echo tablerow_kpilist_new();
+}
+echo end_div();
+
+
+?>
