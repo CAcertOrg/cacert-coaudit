30b15e67dd935af15a415d7813e4ff4003544074
inopiae <inopiae@cacert.org>
2014-11-25 17:32:05 +0100

    master: updated session save

--- www/index.php
+++ www/index.php
@@ -195,23 +195,35 @@ if ($type == 'sessionlist') {
 if ($type == 'session') {
     $continue=true;
     if (isset( $_REQUEST['new']) || isset( $_REQUEST['edit'])) {
+        $error = '';
         $sid = array_key_exists('sid',$_REQUEST) ? intval($_REQUEST['sid']) : '';
         $session_name = array_key_exists('session_name',$_REQUEST) ? tidystring($_REQUEST['session_name']) : '';
         $from = array_key_exists('from',$_REQUEST) ? tidystring($_REQUEST['from']) : '';
         $to = array_key_exists('to',$_REQUEST) ? tidystring($_REQUEST['to']) : '';
+        if (!validdate($from)) {
+            $error = 'Problem with from date </br>';
+        }
+        if (!validdate($to)) {
+            $error .= 'Problem with to date </br>';
+        }
         $activetest = array_key_exists('active',$_REQUEST) ? tidystring($_REQUEST['active']) : '';
         if ($activetest == "'on'") {
             $active = 1;
         } else {
             $active = 0;
         }
-        if (isset( $_REQUEST['new'])){
-            $db -> insert_session($session_name, $from, $to );
+        if ($error == '') {
+            if (isset( $_REQUEST['new'])){
+                $db -> insert_session($session_name, $from, $to );
+            } else {
+                $db -> update_session($session_name, $from, $to, $active, $sid);
+            }
+            include('../forms/sessionlist.php');
+            $continue=false;
         } else {
-            $db -> update_session($session_name, $from, $to, $active, $sid);
+            echo error($error);
         }
-        include('../forms/sessionlist.php');
-        $continue=false;
+
     }
     if (isset( $_REQUEST['sid'])) {
         $_SESSION['user']['sid'] = $_REQUEST['sid'];
