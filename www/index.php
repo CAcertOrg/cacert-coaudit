b97ca4709250446a6867e91de9d49e2006414699
inopiae <inopiae@cacert.org>
2014-11-26 08:32:37 +0100

    master: changes due to new db column in table coaudit_session

--- www/index.php
+++ www/index.php
@@ -201,10 +201,10 @@ if ($type == 'session') {
         $from = array_key_exists('from',$_REQUEST) ? tidystring($_REQUEST['from']) : '';
         $to = array_key_exists('to',$_REQUEST) ? tidystring($_REQUEST['to']) : '';
         if (!validdate($from)) {
-            $error = 'Problem with from date </br>';
+            $error = _('Problem with from date') . '</br>';
         }
         if (!validdate($to)) {
-            $error .= 'Problem with to date </br>';
+            $error .= _('Problem with to date') . '</br>';
         }
         $activetest = array_key_exists('active',$_REQUEST) ? tidystring($_REQUEST['active']) : '';
         if ($activetest == "'on'") {
@@ -212,11 +212,18 @@ if ($type == 'session') {
         } else {
             $active = 0;
         }
+        $defaulttest = array_key_exists('default',$_REQUEST) ? tidystring($_REQUEST['default']) : '';
+        if ($defaulttest == "'on'") {
+            $default = 1;
+        } else {
+            $default = 0;
+        }
+
         if ($error == '') {
             if (isset( $_REQUEST['new'])){
-                $db -> insert_session($session_name, $from, $to );
+                $db -> insert_session($session_name, $from, $to, $default );
             } else {
-                $db -> update_session($session_name, $from, $to, $active, $sid);
+                $db -> update_session($session_name, $from, $to, $default, $active, $sid);
             }
             include('../forms/sessionlist.php');
             $continue=false;
