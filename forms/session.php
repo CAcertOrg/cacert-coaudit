9d5dbe8cda5a8ed92bc5f876ab86cec8ac49d4b6
inopiae <inopiae@cacert.org>
2014-11-26 08:28:49 +0100

    master: changes due to new db column in table coaudit_session

--- forms/session.php
+++ forms/session.php
@@ -21,6 +21,7 @@ if ($sid == 0) {
     $session_name = '';
     $from = '';
     $to = '';
+    $default = 0;
     $active = 0;
 } else {
     //edit session
@@ -29,6 +30,7 @@ if ($sid == 0) {
     $session_name = $sessions['session_name'];
     $from = $sessions['from'];
     $to = $sessions['to'];
+    $default = $sessions['default'];
     $active = $sessions['active'];
 }
 
@@ -48,6 +50,7 @@ echo tableheader(_('Session'), 2);
 echo tablerow_2col_textbox(_('Session'), 'session_name', $session_name);
 echo tablerow_2col_textbox(_('From'), 'from', $from);
 echo tablerow_2col_textbox(_('To'), 'to', $to);
+echo tablerow_topics_checkbox('default', _('Default'), $default);
 echo tablerow_topics_active($active);
 echo tablefooter_user(2, $sid, $writeperm);;
 echo built_form_footer($hidden);
