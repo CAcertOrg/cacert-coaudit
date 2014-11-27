1e7c6b6e5f45a050af8785cfcda8045c9c9c3a23
inopiae <inopiae@cacert.org>
2014-11-27 09:27:12 +0100

    master: corrected write_permission

--- module/basic_functions.php
+++ module/basic_functions.php
@@ -68,7 +68,7 @@ function get_write_permission( $view){
         return 0;
     }
     $writeview = $view_perm['write_permission'];
-    if ((intval($_SESSION['user']['read_permission']) & intval($writeview))>0) {
+    if ((intval($_SESSION['user']['write_permission']) & intval($writeview))>0) {
         return 1;
     } else {
         return 0;
