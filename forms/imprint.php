4a6973d5c60cd89565671e1b2e9e40d02726370b
inopiae <inopiae@cacert.org>
2014-11-28 08:12:53 +0100

    master: correction of positions

--- forms/imprint.php
+++ forms/imprint.php
@@ -4,9 +4,9 @@
 <h2>Imprint</h2>
 <h3>Postal Address</h3>
 <p>
-	CAcert Inc.<br /> 
-	P.O. Box 66<br /> 
-	Oatley NSW 2223<br /> 
+	CAcert Inc.<br />
+	P.O. Box 66<br />
+	Oatley NSW 2223<br />
 	Australia
 </p>
 
@@ -14,11 +14,11 @@
 
 <h3>CAcert Inc. Committee</h3>
 
-<p>xx (President)<br/>
-xx (Vice President)<br/>
-Michael Tänzer (Treasurer)<br/>
+<p>Dirk Astrath (President)<br/>
+J&uumlrgen Bruckner (Vice President)<br/>
+Michael T&aumlnzer (Treasurer)<br/>
 Etienne Ruedin (Secretary)<br/>
-Jürgen Bruckner, Benjamin Ball, Dirk Astrath, Guillaume Romagny, Robert Cruisnak </p>
+Benjamin Ball, Guillaume Romagny, Robert Cruisnak </p>
 
 <p>This website is part of the community project <a href="http://cacert.org">CAcert.org</a>.</p>
 
