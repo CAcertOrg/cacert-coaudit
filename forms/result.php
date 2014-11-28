34cc8ba9bdfa0f7e9b8e413c05e214c37d9a5a37
inopiae <inopiae@cacert.org>
2014-11-28 08:29:56 +0100

    master: added possibility to review an result entry and to delete it if right is auditor

--- forms/result.php
+++ forms/result.php
@@ -14,29 +14,60 @@ if ($readperm == 0) {
     exit;
 }
 
-$primaryemail = '';
-$isassurer = '';
-$expierencepoints = '';
-$country = '';
-$location = '';
-$coauditdate = '';
 
-$rid = 0;
 
+if (isset($_SESSION['user']['rid'])) {
+    $rid = intval($_SESSION['user']['rid']);
+    $writeperm = 0;
+} else {
+    $rid = 0;
+}
+if (check_role_access('Auditor', $_SESSION['user']['write_permission'])) {
+    $writeperm = 2;
+}
+
+if ($rid == 0) {
+    $primaryemail = '';
+    $isassurer = '';
+    $expierencepoints = '';
+    $country = '';
+    $location = '';
+    $coauditdate = '';
+    //get last session
+    if (array_key_exists('coaudit_session',$_SESSION['user'])) {
+        $coaudit_session_id = $_SESSION['user']['coaudit_session'];
+    } else {
+        $cs_res= $db -> get_all_session('WHERE `default` = 1');
+        $coaudit_session_id = $cs_res['session_id'];
+    }
+    //get data
+    $questions = $db -> get_all_sessiontopics(' `st`.`coaudit_session_id` = ' . $coaudit_session_id . ' and `st`.`active` = 1');
+} else {
+
+    $user = $db -> get_result_user($rid);
+    $primaryemail = $user['primaryemail'];
+    $isassurer = $user['assurer'];
+    $expierencepoints = $user['expierencepoints'];
+    $country = $user['country'];
+    $location = $user['location'];
+    $coauditdate = $user['coauditdate'];
+
+    $coaudit_session_id = $_SESSION['user']['coaudit_session'];
+    $questions = $db -> get_result_topics($coaudit_session_id,$rid);
+}
 
 $hidden[] = array('rid', $rid);
 
 $sessionres = $db -> get_all_session();
-$coaudit_session_id = 2; //get last session
+
+
 //buildform
 echo start_div('content');
 
-//get data
-$questions = $db -> get_all_sessiontopics(' `st`.`coaudit_session_id` = ' . $coaudit_session_id . ' and `st`.`active` = 1');
 
 echo built_form_header('../index.php?type=result');
 echo tableheader(_('Enter coaudit result'), 2);
-echo tablerow_2col_dropbox(_('Coaudit session'), $sessionres, $coaudit_session_id, 'session_id', 'session_name', 0);
+echo tablerow_2col_dropbox_apply(_('Coaudit session'), $sessionres, $coaudit_session_id, 'session_id', 'session_name', 'change', _('Update'), 0);
 echo tablerow_2col_textbox(_('Assurer mail'), 'primaryemail', $primaryemail);
 echo tablerow_2col_checkbox(_('Is assuerer?'), 'assurer', $isassurer);
 echo tablerow_2col_textbox(_('Expierence Points'), 'expierencepoints', $expierencepoints);
@@ -46,13 +77,19 @@ echo tablerow_2col_textbox(_('Date of coaudit'), 'coauditdate', $coauditdate);
 
 $i = 1;
 foreach($questions as $row){
+    $r = '';
+    $c = '';
+    if ($rid !=0) {
+        $r = intval($row['result']);
+        $c = $row['comment'];
+    }
     echo tablerow_label('Q' . $row['topic_no'] . ' ' .$row['session_topic'], 2);
-    echo tablerow_2col_checkbox(_('Result (passed - tick)'), 'r'. $i, '');
-    echo tablerow_2col_textbox(_('Comment'), 'c'. $i,  '');
+    echo tablerow_2col_checkbox(_('Result (passed - tick)'), 'r'. $i, $r);
+    echo tablerow_2col_textbox(_('Comment'), 'c'. $i, $c);
     $hidden[]=array('qid' . $i, $row['session_topic_id']);
     $i +=1;
 }
-echo tablefooter_user(2, $rid, $writeperm);;
+echo tablefooter_user(2, $rid, $writeperm);
 echo built_form_footer($hidden);
 echo end_div();
 
