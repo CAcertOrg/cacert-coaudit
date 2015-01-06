b72e9988d1c30cc74c3780860c1d23e14cc6b23c
inopiae <inopiae@cacert.org>
2015-01-06 10:00:00 +0100

    master: rework statistics output

--- forms/statistic.php
+++ forms/statistic.php
@@ -9,51 +9,62 @@ $db = new db_function();
 $readperm = get_read_permission('statistic');
 $writeperm = get_write_permission('statistic');
 
-$res = $db -> get_statiscs_basic();
-
-$year = 0;
-$session = '';
-$sessionold = '';
-$sessionhead = '';
-$rowheader1 = '';
-$rowheader2 = '';
-$datarow = '';
-$col = 0;
-$start = 0;
 $kpidata = array();
+
+$ressessions = $db -> get_statisics_sessions();
 //buildform
 echo start_div('content');
 
-foreach($res as $row){
-    if ($session != $row['Session'] ) {
-        $sessionold = $session;
-        $session = $row['Session'];
-        if ($start == 0 ) {
+foreach ($ressessions as $ressession) {
+
+    //build seesion table
+    $res = $db -> get_statiscs_basic(' and `r`.`coauditsession_id` = ' . $ressession['session_id']);
+
+    $year = 0;
+    $session = '';
+    $sessionold = '';
+    $sessionhead = '';
+    $rowheader1 = '';
+    $rowheader2 = '';
+    $datarow = '';
+    $col = 0;
+    $start = 0;
+
+        foreach($res as $row){
+        if ($session != $row['Session'] ) {
             $sessionold = $session;
+            $session = $row['Session'];
+            if ($start == 0 && $col == 0) {
+                $sessionold = $session;
+            }
+            $start = 0;
         }
-        $start = 0;
-    }
-    if ($year != $row['CYear'] ) {
-        if ($col >0 && $start == 0) {
-            echo tableheader(sprintf(_('Coaudit results for %s'), $sessionold), $col);
-            echo tablerow_start() . $rowheader1 . tablerow_end();
-            echo tablerow_start() . $rowheader2 . tablerow_end();
-            $start = 1;
+        if ($year != $row['CYear'] ) {
+            if ($col >0 && $start == 0) {
+                if ($sessionold != $session) {
+                    echo tablerow_start() . $datarow . tablerow_end();
+                    echo table_end();
+                    echo empty_line();
+                }
+                echo tableheader(sprintf(_('Coaudit results for %s'), $sessionold), $col);
+                echo tablerow_start() . $rowheader1 . tablerow_end();
+                echo tablerow_start() . $rowheader2 . tablerow_end();
+                $start = 1;
+                $col = 1;
+            }
+            if ($col >0 && $sessionold == $session ) {
+                echo tablerow_start() . $datarow . tablerow_end();
+            }
+            $year = $row['CYear'];
+            $datarow = tablecell($row['CYear']);
+            $datarow .= tablecell($row['Total']);
+            $rowheader1 = tablecell('');
+            $rowheader1 .= tablecell('');
+            $rowheader2 = tablecell(_('Year'));
+            $rowheader2 .= tablecell(_('Tests'));
+            $kpidata[] = array($row['CYear'],$row['Total']);
+            $col = 2;
         }
-        if ($col >0 ) {
-            echo tablerow_start() . $datarow . tablerow_end();
-        }
-        $year = $row['CYear'];
-        $datarow = tablecell($row['CYear']);
-        $datarow .= tablecell($row['Total']);
-        $rowheader1 = tablecell('');
-        $rowheader1 .= tablecell('');
-        $rowheader2 = tablecell(_('Year'));
-        $rowheader2 .= tablecell(_('Tests'));
-        $kpidata[] = array($row['CYear'],$row['Total']);
-        $col = 2;
-    }
-    if ($start == 0) {
         if ($_SESSION['user']['read_permission']>1) {
              $rowheader1 .= tablecell($row['Topic'],2);
         } else {
@@ -61,47 +72,50 @@ foreach($res as $row){
         }
         $rowheader2 .= tablecell(_('Passed'));
         $rowheader2 .= tablecell(_('Percentage'));
+        $datarow .= tablecell($row['res']);
+        $datarow .= tablecell(number_format($row['Perc'], 1, '.', '') . '%');
+        $col +=2;
     }
-    $datarow .= tablecell($row['res']);
-    $datarow .= tablecell(number_format($row['Perc'], 1, '.', '') . '%');
-//    $datarow .= tablecell($row['Perc'] . '%');
-    $col +=2;
-}
-if ($start == 0) {
-    echo tableheader(sprintf(_('Coaudit results for %'), $sessionold), $col);
-    echo tablerow_start() . $rowheader1 . tablerow_end();
-    echo tablerow_start() . $rowheader2 . tablerow_end();
-    $start = 1;
-}
-echo tablerow_start() . $datarow . tablerow_end();
-echo table_end();
-
-echo empty_line();
-echo empty_line();
-
-$res = $db -> get_statiscs_kpi(2);
-echo tableheader(_('Coaudit KPI'),5);
-$rowheader = tablerow_start();
-$rowheader .= tablecell(_('Year'));
-$rowheader .= tablecell(_('Tests'));
-$rowheader .= tablecell(_('Assurances'));
-$rowheader .= tablecell(_('Percentage'));
-$rowheader .= tablecell(_('Taget KPI')) . tablerow_end();
-echo $rowheader;
-foreach($res as $row){
-    $stest = 0;
-    foreach ($kpidata as $syear) {
-        if ($syear[0] == $row['session_year']) {
-            $stest = $syear[1];
-        }
+    if ($start == 0) {
+        echo tableheader(sprintf(_('Coaudit results for %'), $sessionold), $col);
+        echo tablerow_start() . $rowheader1 . tablerow_end();
+        echo tablerow_start() . $rowheader2 . tablerow_end();
+        $start = 1;
     }
-    $datarow = tablecell($row['session_year']);
-    $datarow .= tablecell($stest);
-    $datarow .= tablecell($row['assurances']);
-    $datarow .= tablecell(number_format(($stest / $row['assurances']) * 100, 1, '.', '') . '%');
-    $datarow .= tablecell(number_format($row['target'], 1, '.', '') . '%');
     echo tablerow_start() . $datarow . tablerow_end();
+    echo table_end();
+    echo empty_line();
+
+
+    //KPI statistic
+    echo empty_line();
+
+
+    $res = $db -> get_statiscs_kpi( $ressession['session_id']);
+    echo tableheader(_('Coaudit KPI for ' . $ressession['session_name']),5);
+    $rowheader = tablerow_start();
+    $rowheader .= tablecell(_('Year'));
+    $rowheader .= tablecell(_('Tests'));
+    $rowheader .= tablecell(_('Assurances'));
+    $rowheader .= tablecell(_('Percentage'));
+    $rowheader .= tablecell(_('Taget KPI')) . tablerow_end();
+    echo $rowheader;
+    foreach($res as $row){
+        $stest = 0;
+        foreach ($kpidata as $syear) {
+            if ($syear[0] == $row['session_year']) {
+                $stest = $syear[1];
+            }
+        }
+        $datarow = tablecell($row['session_year']);
+        $datarow .= tablecell($stest);
+        $datarow .= tablecell($row['assurances']);
+        $datarow .= tablecell(number_format(($stest / $row['assurances']) * 100, 1, '.', '') . '%');
+        $datarow .= tablecell(number_format($row['target'], 1, '.', '') . '%');
+        echo tablerow_start() . $datarow . tablerow_end();
+    }
+    echo table_end();
+    echo empty_line();
 }
-echo table_end();
 echo end_div();
 ?>
